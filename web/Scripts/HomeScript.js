80abf6cf4a98dcce5c9b700f480d09c29d62ab99
AnisaEbadi <ebadi.anisa@gmail.com>
2016-07-24 09:23:21 +0430

    workhistory in profile and single page for home and faq and about us

---
+++ web/Scripts/HomeScript.js
@@ -0,0 +1,17 @@
+﻿var app = angular.module("myApp", ["ngRoute"]);
+app.config(function ($routeProvider) {
+    $routeProvider
+    .when("/", {
+        templateUrl: "Home2.html"
+    })
+    .when("/About", {
+        templateUrl: "AboutUs.html"
+    })
+    .when("/FAQ", {
+        templateUrl: "FAQ.html"
+    })
+    //.when("/profile", {
+    //    templateUrl: "profile.html"
+    //})
+
+});
--- web/Scripts/ProfileScript.js
+++ web/Scripts/ProfileScript.js
@@ -17,10 +17,16 @@ myapp.controller("controller",
             $scope.skills = $scope.selectedService.skills;
         }; // function()
 
+        
         $scope.dates = [];
         for (var i = 1970; i <= 2020; i++) {
             $scope.dates.push(i);
         }
+
+        
+        //$scope.months = [{ id: 1, name: "January" }, { id: 2, name: "February" }, { id: 3, name: "March" }, { id: 4, name: "April" }, { id: 5, name: "May" }, { id: 6, name: "June" }, { id: 6, name: "July" }
+        //    , { id: 7, name: "August" }, { id: 8, name: "September" }, { id: 10, name: "September" }, { id: 11, name: "November" }, { id: 12, name: "December" }];
+        $scope.months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "September", "November", "December"];
 
         var degrees = $resource("/degrees")
 
@@ -30,8 +36,87 @@ myapp.controller("controller",
           } // function(data)
         ) // service.query
 
+        //**************************
+        // Work History
+        //*************************
+        $scope.locations = ["location1", "location2", "location3"];
+        //$scope.roles = ["Intern", "Individual Contributor", "Lead", "Manager", "Executive", "Owner"];
+        $scope.roles = [{ value: "I", name: "Intern" }, { value: "C", name: "Individual Contributor" }, { value: "L", name: "Lead" }, { value: "M", name: "Manager" }, { value: "E", name: "Executive" }, {value:"O",name:"Owner"}];
+
+        var saveWhRes = $resource("/saveWork")
+        $scope.addWorkHistory = function () {
+            var saveWh = new saveWhRes();
+            saveWh.id = $scope.whId;
+            saveWh.company = $scope.whCompany;
+            saveWh.location = $scope.whLocation;
+            saveWh.title = $scope.whTitle;
+            saveWh.role = $scope.whRole; /*$scope.whRole;*/
+            saveWh.frommonth =/*$scope.whFromMonth.id;*/$scope.months.indexOf($scope.whFromMonth)+1;
+            saveWh.fromyear = $scope.whFromYear;
+            saveWh.tomonth = /*$scope.whToMonth.id;*/$scope.months.indexOf($scope.whToMonth)+1;
+            saveWh.toyear = $scope.whToYear;
+            saveWh.currently = $scope.whCurrently;
+            saveWh.description = $scope.whDesc;
+            
+            var index;
+            $scope.account.works.some(function (elem, i) {
+                return elem.id === $scope.whId ? (index = i, true) : false;
+            });
+
+            saveWh.$save(function (wh) {
+                if (index > 0)
+                    $scope.account.works[index] = wh;
+                else
+                    $scope.account.works.push(wh);
+                
+                cleanHistoryInputs();
+            });
+        }
+
+        $scope.editWorkHistory = function (wh) {
+            
+            $scope.whId = wh.id;
+            $scope.whCompany = wh.company;
+            $scope.whLocation = wh.location;
+            $scope.whTitle = wh.title;
+            $scope.whRole = wh.role.value;
+            $scope.whFromMonth = wh.frommonth.name;
+            $scope.whFromYear = wh.fromyear;
+            $scope.whToMonth = wh.tomonth.name;
+            $scope.whToYear = wh.toyear;
+            $scope.whCurrently = wh.currently;
+            $scope.whDesc = wh.description;
+        }
+
+        var delWhRes = $resource("/deleteWork")
+        $scope.deleteWorkHistory = function (wh) {
+            var delWh = new delWhRes();
+            delWh.id = wh.id;
+            delWh.$save(function (dwr) {
+                $scope.account.works.splice($scope.account.works.indexOf(dwr), 1);
+                cleanHistoryInputs();
+            });
+        }
         
-        var saveEduRes = $resource("/SaveEducation")
+        function cleanHistoryInputs()
+        {
+            $scope.whId = "";
+            $scope.whCompany = "";
+            $scope.whLocation = "";
+            $scope.whTitle = "";
+            $scope.whRole = [];
+            $scope.whFromMonth = [];
+            $scope.whFromYear = "";
+            $scope.whToMonth = [];
+            $scope.whToYear = "";
+            $scope.whCurrently = "";
+            $scope.whDesc = "";
+        }
+
+        //*************************
+        // education
+        //*************************
+        var saveEduRes = $resource("/saveEducation")
         $scope.addEducation = function () {
             var saveEdu = new saveEduRes();
             saveEdu.school = $scope.School;
