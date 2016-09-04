b456670c0ae6388775a1c47c4c98e2e14b7cd498
sinanazemi <sina.nazemi@gmail.com>
2016-09-04 16:19:51 +0430

    sending MainService images to browser

--- model/mainService.go
+++ model/mainService.go
@@ -12,6 +12,9 @@ type MainService struct{
   Question string `json:"question"`
   IsSelected bool `json:"isselected"`
   Skills []Skill `json:"skills"`
+
+  UnselectImageURL string `json:"unselectimageurl"`
+  SelectImageURL string `json:"selectimageurl"`
 }
 
 func getMainServices() []MainService {
@@ -19,7 +22,13 @@ func getMainServices() []MainService {
   // list of all of services
 	var result = make([]MainService, 0)
 
-  services, err := util.Select(readMainService, "select * from MainService")
+  query :=
+    "select m.*, COALESCE(i1.ID, 0), COALESCE(i2.ID, 0) from MainService m " +
+    "left outer join Image i1 on i1.category = 'MS1u' and i1.ParentID = m.ID " +
+    "left outer join Image i2 on i2.category = 'MS1s' and i2.ParentID = m.ID "
+
+
+  services, err := util.Select(readMainService, query)
 
   if err != nil {
     return result
@@ -42,9 +51,14 @@ func getMainServices() []MainService {
 
 func readMainService(rows *sql.Rows) (interface{}, error) {
 
+  var idu, ids int
+
   var service MainService = MainService{}
-  err := rows.Scan(&service.Id, &service.Name, &service.Question)
+  err := rows.Scan(&service.Id, &service.Name, &service.Question, &idu, &ids)
   service.IsSelected = false
+
+  service.UnselectImageURL = util.GetImageURL(idu)
+  service.SelectImageURL = util.GetImageURL(ids)
 
   return service, err
 }
